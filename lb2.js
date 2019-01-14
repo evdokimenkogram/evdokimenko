773c3027abb4c68f9983170e5692428b2128f0be
evdokimenkogram <39723856+evdokimenkogram@users.noreply.github.com>
2019-01-14 10:16:17 +0200

    Create lb2.js

---
+++ lb2.js
@@ -0,0 +1,16 @@
+function f4()
+{
+document.getElementById("T5").src="04.jpg";
+}
+function f3()
+{
+document.getElementById("T5").src="03.png";
+}
+function f2()
+{
+document.getElementById("T5").src="01.gif";
+}
+function f1()
+{
+document.getElementById("T5").src="02.jpg";
+}
